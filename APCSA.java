fb2a7bb1b79db05767f2f7871773d85844f345ef
garv sachdev <83745829+gavkujo@users.noreply.github.com>
2022-03-31 23:20:35 +0530

    Add files via upload

---
+++ APCSA.java
@@ -0,0 +1,27 @@
+/*
+Programmer: Garv Sachdev
+Date: 28/3/2022
+Prupose: Basic Stuff and some stuff that are different than Python that I need to remember.
+*/
+public class APCSA {
+    public static void main (String[] args) {
+        System.out.print("AP");
+        System.out.print("CS");
+        System.out.println("A");
+        System.out.println("sossy");
+        System.out.println("Hello World");
+        /*
+        Literals and String Literals
+        */
+        System.out.println(3);
+        System.out.println("3");
+        /*
+        Double and Integer interactions
+        */
+        System.out.println(17+5);/* output is a int */ 
+        System.out.println(17.0+5.0);/* output is a double */ 
+        System.out.println(17.0+5);/* output is a double */ 
+        System.out.println((double) 6/4);
+        System.out.println((int)2.5+3.5);
+    }
+}
